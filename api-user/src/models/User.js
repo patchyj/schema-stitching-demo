ce79b9c90304f72265b48191b669b3b699759898
patchyj <jackjwmcgregor@gmail.com>
2019-03-23 17:40:44 +0000

    removed test suite

--- api-user/src/models/User.js
+++ api-user/src/models/User.js
@@ -5,42 +5,42 @@ const { Schema, model } = mongoose;
 
 // Create Schema
 const UserSchema = new Schema({
-	firstName: {
-		type: String
-	},
-	lastName: {
-		type: String
-	},
-	email: {
-		type: String
-	},
-	password: {
-		type: String
-	},
-	bio1: {
-		type: String
-	},
-	bio2: {
-		type: String
-	},
-	bio3: {
-		type: String
-	},
-	tagline: {
-		type: String
-	},
-	avatar: {
-		type: String
-	},
-	linkedinInURL: {
-		type: String
-	},
-	twitterURL: {
-		type: String
-	},
-	facebookURL: {
-		type: String
-	}
+  firstName: {
+    type: String
+  },
+  lastName: {
+    type: String
+  },
+  email: {
+    type: String
+  },
+  password: {
+    type: String
+  },
+  bio1: {
+    type: String
+  },
+  bio2: {
+    type: String
+  },
+  bio3: {
+    type: String
+  },
+  tagline: {
+    type: String
+  },
+  avatar: {
+    type: String
+  },
+  linkedinInURL: {
+    type: String
+  },
+  twitterURL: {
+    type: String
+  },
+  facebookURL: {
+    type: String
+  }
 });
 
 export default model('users', UserSchema);
--- api-user/src/resolver/userResolver.js
+++ api-user/src/resolver/userResolver.js
@@ -8,63 +8,63 @@ dotenv.config();
 mongoose.Promise = require('bluebird');
 
 mongoose.connect(
-	process.env.USER_DB,
-	{ useNewUrlParser: true }
+  process.env.USER_DB,
+  { useNewUrlParser: true }
 );
 
 // add some small resolvers
 const resolvers = {
-	Query: {
-		allUsers: async () => {
-			const users = await User.find();
+  Query: {
+    allUsers: async () => {
+      const users = await User.find();
 
-			return users.reverse();
-		},
-		user: async (parent, { id }) => {
-			const user = await User.findById(id);
-			return user;
-		}
-	},
-	Mutation: {
-		// ========= CREATE =========
-		addUser: async (parent, user) => {
-			const existingUser = await User.findOne({ email: user.email });
+      return users.reverse();
+    },
+    user: async (parent, { id }) => {
+      const user = await User.findById(id);
+      return user;
+    }
+  },
+  Mutation: {
+    // ========= CREATE =========
+    addUser: async (parent, user) => {
+      const existingUser = await User.findOne({ email: user.email });
 
-			if (existingUser !== null) {
-				return new UserInputError('This email is already being used');
-			}
+      if (existingUser !== null) {
+        return new UserInputError('This email is already being used');
+      }
 
-			const newUser = new User({
-				firstName: user.firstName,
-				lastName: user.lastName,
-				email: user.email,
-				password: user.password
-			});
+      const newUser = new User({
+        firstName: user.firstName,
+        lastName: user.lastName,
+        email: user.email,
+        password: user.password
+      });
 
-			const salt = await bcrypt.genSalt(10);
-			const hash = await bcrypt.hash(newUser.password, salt).catch((err) => {
-				throw new UserInputError(err);
-			});
+      const salt = await bcrypt.genSalt(10);
+      const hash = await bcrypt.hash(newUser.password, salt).catch((err) => {
+        throw new UserInputError(err);
+      });
 
-			newUser.password = hash;
-			newUser.save();
+      newUser.password = hash;
+      newUser.save();
 
-			return newUser;
-		},
-		updateUser: async (parent, user) => {
-			const updatedUser = await User.findOneAndUpdate(user.id, user, {
-				new: true
-			});
+      return newUser;
+    },
+    updateUser: async (parent, user) => {
+      const updatedUser = await User.findOneAndUpdate(user.id, user, {
+        new: true
+      });
 
-			return updatedUser;
-		},
-		deleteUser: async (parent, user) => {
-			await User.findOneAndDelete({ _id: user.id });
-			const ifUser = await User.findOne({ id: user.id }) ? 'Something went wrong' : 'User successfully deleted';
+      return updatedUser;
+    },
+    deleteUser: async (parent, user) => {
+      await User.findOneAndDelete({ _id: user.id });
+      const ifUser = await User.findOne({ id: user.id }) ? 'Something went wrong' : 'User successfully deleted';
 
-			return ifUser;
-		}
-	}
+      return ifUser;
+    }
+  }
 };
 
 export default resolvers;
